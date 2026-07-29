b8fbd77bb8e748f84757e48a3c73324701678872
yusufchota <yusufchota7@gmail.com>
2026-07-29 11:28:16 +0530

    path case sensitive

--- src/Routes/AppRoutes.jsx
+++ src/Routes/AppRoutes.jsx
@@ -1,8 +1,8 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 
-import Signup from '../pages/Signup';
-import Login from '../pages/Login';
-import Home from '../pages/Home';
+import Signup from '../Pages/Signup';
+import Login from '../Pages/Login';
+import Home from '../Pages/Home';
 
 export default function AppRoutes() {
   return (
